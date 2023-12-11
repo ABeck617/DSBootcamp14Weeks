447240fc89ee0180b02a993435eb8659c7f084ed
Anthony Beckford <abeckford532@gmail.com>
2023-12-11 12:28:40 -0500

    Add files via upload
    Day 1 Practice and Solution

---
+++ Day1.Playgrounds/Day1Challenge.playground/Contents.swift
@@ -0,0 +1,20 @@
+import Cocoa
+
+// 1. Declare a String constant named welcomeMessage with the value "Welcome to Swift!"
+
+let welcomeMessage: String = "Welcome to Swift"
+
+
+// 2. Declare an Int variable named userAge and initialize it with your age.
+
+var userAge: Int = 33
+
+// 3. Declare a Double constant named averageScore and initialize it with any decimal value.
+
+let averageScore: Double = 30.5
+
+// 4. Print out all these variables with a message describing them, using type annotations to explicitly set their types.
+
+print("Welcome Message: \(welcomeMessage)")
+print("User Age: \(userAge)")
+print("Aveage Score: \(averageScore)")
---
+++ Day1.Playgrounds/MyPlayground.playground/Contents.swift
@@ -0,0 +1,18 @@
+import SwiftUI
+
+
+// use let if you dont want the variable to change (constant)
+let name: String = "Anthony Beckford"
+
+
+// use var if you want variable to change (variable)
+print(name)
+var age: Int = 32
+
+print(age)
+age = 33
+
+
+var isOpen: Bool = true
+
+print(isOpen)
